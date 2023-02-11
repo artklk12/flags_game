8414647d0941bcb922397506bac54e033c0f197a
artklk12 <67318020+artklk12@users.noreply.github.com>
2023-02-12 00:17:58 +0300

    Update db.py

--- core/db.py
+++ core/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:VCfyJqOKU7K64urnaUPLpB1lHxZVlEXu@dpg-cfjvad9mbjsn9ea1t83g-a/flags"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:VCfyJqOKU7K64urnaUPLpB1lHxZVlEXu@dpg-cfjvad9mbjsn9ea1t83g-a.frankfurt-postgres.render.com/flags"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
